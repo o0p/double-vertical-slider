c57d08bc38ae5ad05c1b2b6d842eb1a2ca5fdb5a
Russell <russelldev@protonmail.com>
2021-01-27 18:15:50 +0000

    Add select vars

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const uppButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLenght = slideRight.querySelectorAll('div').length;
 
+let activeSlideIndex = 0;
